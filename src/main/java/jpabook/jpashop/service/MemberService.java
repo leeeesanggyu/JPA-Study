98baf2f6d4c95fea443cab2184ec0694a5e20c8f
salgu1998 <salgu1998@gmail.com>
2022-02-21 01:46:34 +0900

    add : 회원등록 API

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -19,8 +19,7 @@ public class MemberService {
      * 회원 가입
      */
     @Transactional
-    public Long join(Member member) {
-
+    public Long  join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
